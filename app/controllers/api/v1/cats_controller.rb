f473fd47d0f71f396dad53a9bf7ccd1b087ffbce
Cleiviane Costa <costa.cleiviane@gmail.com>
2017-09-01 09:12:55 -0300

    Created app background

---
+++ app/controllers/api/v1/cats_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::CatsController < ApplicationController
+
+	def index
+		render json: Cat.includes(:hobbies), include: ['hobbies']
+	end
+
+end
---
+++ app/controllers/api/v1/hobbies_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::HobbiesController < ApplicationController
+
+	def index
+		render json: Hobby.includes(:cats), include: ['cats']
+	end
+
+end
